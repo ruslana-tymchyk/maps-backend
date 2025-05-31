5e32cf43d7b848da0a41cee0f040a738c3dc7f79
ruslana-tymchyk <tymchyk3@gmail.com>
2025-05-31 15:38:21 +0100

    adding port variable

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 from routes.supabase_routes import supabase_bp
 from routes.chat_routes import chat_bp
@@ -8,7 +9,6 @@ from flask_app.extensions import limiter
 
 # Create a Flask app
 app = Flask(__name__)
-
 
 allowed_origins = [
     'http://localhost:3000',           # Local development
@@ -23,6 +23,7 @@ app.register_blueprint(supabase_bp)
 app.register_blueprint(chat_bp)
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
     
 
